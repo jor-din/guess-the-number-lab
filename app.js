9c671da47258da736ca77dfadbfcecb5ac2f9cd4
jordinhuang <jordinhuang@gmail.com>
2022-05-28 01:21:09 -0400

    add player, getguess, and render methods

--- app.js
+++ app.js
@@ -6,13 +6,38 @@ const game = {
     prevGuesses: [],
     
     play: function() {
-      this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+        this.secretNum = Math.floor(Math.random() * 
+        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+
+        do {
+            this.prevGuesses.push(this.getGuess());
+            this.render();
+        }
+        while (this.prevGuesses[this.prevGuesses.length-1] !== this.secretNum)
+        return render();
     },
 
     getGuess: function(){
-      const playerGuess = parseInt(prompt(`enter a guess between `))
+        let playerGuess;
+     do {
+        playerGuess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum} `))
+     }
+      while (isNaN(playerGuess) || playerGuess < this.smallestNum || playerGuess > this.biggestNum)
+      return playerGuess;
+      },
+
+    render: function() {
+        let message;
+        if (this.prevGuesses[this.prevGuesses.length-1] === this.secretNum){
+            message = `Congrats! You guessed the number in ${this.prevGuesses.length}!`
+        } else if (this.prevGuesses[this.prevGuesses.length-1] > this.secretNum) {
+            message = `Your guess it too high! Previous guess: ${this.prevGuesses.join(' ,')}`
+        }else if (this.prevGuesses[this.prevGuesses.length-1] < this.secretNum){
+            message = `Your guess it too low! Previous guess: ${this.prevGuesses.join(', ')}`
         
+    }
+    alert(message);
+  
     } 
-  }
+}
 
